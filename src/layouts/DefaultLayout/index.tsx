fab6bcb6370b00d5484a499bcd615c819b77e22d
Matheus Valentim <meduardo.valentim@gmail.com>
2023-02-03 00:23:49 -0300

    Styled Header w/ layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
