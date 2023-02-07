f0c86d73fa86f359a9b766e14404faf33ae502c4
Matheus Valentim <meduardo.valentim@gmail.com>
2023-02-06 22:11:37 -0300

    Finished context routes and reseting form

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
 
       <HistoryList>
         <table>
@@ -11,57 +17,65 @@ export function History() {
             <tr>
               <th>Tarefa</th>
               <th>Duração</th>
-              <th>Início</th>
+              <th>Duração</th>
               <th>Status</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Estudar React</td>
+              <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>há 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
             <tr>
-              <td>Estudar React</td>
+              <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>há 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
             <tr>
-              <td>Estudar React</td>
+              <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>há 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
             <tr>
-              <td>Estudar React</td>
+              <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>há 2 meses</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
               <td>
                 <Status statusColor="yellow">Em andamento</Status>
               </td>
             </tr>
             <tr>
-              <td>Estudar React</td>
+              <td>Tarefa</td>
               <td>20 minutos</td>
-              <td>há 2 meses</td>
+              <td>Há 2 meses</td>
               <td>
                 <Status statusColor="red">Interrompido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
               </td>
             </tr>
           </tbody>
